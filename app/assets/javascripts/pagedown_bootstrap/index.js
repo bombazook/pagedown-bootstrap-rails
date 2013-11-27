41901693db16490b9506a724366c9e4c237c0a13
Alexander Kostrov <bombazook@gmail.com>
2013-11-27 10:56:23 +0400

    renamed gem in gemspec

--- app/assets/javascripts/pagedown_bootstrap/index.js
+++ app/assets/javascripts/pagedown_bootstrap/index.js
@@ -1,5 +1,5 @@
-//= require markdown.converter
-//= require markdown.localization
-//= require markdown.editor
-//= require markdown.sanitizer
-//= require markdown.extra
+//= require pagedown_bootstrap/markdown.converter
+//= require pagedown_bootstrap/markdown.localization
+//= require pagedown_bootstrap/markdown.editor
+//= require pagedown_bootstrap/markdown.sanitizer
+//= require pagedown_bootstrap/markdown.extra
