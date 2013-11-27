024556d6181c051f583fadecd67edc79bc147876
Alexander Kostrov <bombazook@gmail.com>
2013-11-27 21:26:10 +0400

    changed simple_form input

--- lib/pagedown_bootstrap/simple_form.rb
+++ lib/pagedown_bootstrap/simple_form.rb
@@ -1,10 +1,10 @@
 class PagedownInput < SimpleForm::Inputs::TextInput
+  include ActionView::Helpers::TagHelper
   def input
-    out = "<div id=\"wmd-button-bar-#{attribute_name}\"></div>\n"
-    out << "#{@builder.text_area(attribute_name, input_html_options.merge(
-      { :class => 'wmd-input', :id => "wmd-input-#{attribute_name}"})) }"
+    out = tag(:div, :id=> "wmd-button-bar-#{attribute_name}")
+    out << @builder.text_area(attribute_name, input_html_options.merge(:class => 'wmd-input', :id => "wmd-input-#{attribute_name}"))
     if input_html_options[:preview]
-      out << "<div id=\"wmd-preview-#{attribute_name}\" class=\"wmd-preview\"></div>"
+      out << tag(:div, :id=>"wmd-preview-#{attribute_name}", :class=>"wmd-preview")
     end
     out.html_safe
   end
