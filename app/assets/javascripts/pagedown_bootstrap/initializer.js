a51de8cb7041ef50c2a070aa98c24117b5bf4210
Alexander Kostrov <bombazook@gmail.com>
2013-11-27 14:34:43 +0400

    added initializer

---
+++ app/assets/javascripts/pagedown_bootstrap/initializer.js
@@ -0,0 +1,9 @@
+//= require pagedown_bootstrap/localization
+
+$(document).ready ->
+  $('textarea.wmd-input').each (i, input) ->
+    attr = $(input).attr('id').split('wmd-input')[1]
+    converter = new Markdown.Converter()
+    Markdown.Extra.init(converter)
+    editor = new Markdown.Editor(converter, {strings: window.Markdown.local.rails, helpButton: attr})
+    editor.run()
