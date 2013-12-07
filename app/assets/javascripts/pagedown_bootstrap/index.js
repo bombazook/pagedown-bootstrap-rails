fffbf092f9464e4feaa662ed4172b65f2ddff755
Alexander Kostrov <bombazook@gmail.com>
2013-12-07 17:16:55 +0400

    modified simple form input

--- app/assets/javascripts/pagedown_bootstrap/index.js
+++ app/assets/javascripts/pagedown_bootstrap/index.js
@@ -2,7 +2,5 @@
 //= require pagedown_bootstrap/converter
 //= require pagedown_bootstrap/editor
 //= require pagedown_bootstrap/sanitizer
-//= reuqire pagedown_bootstrap/bootstrap3_customizations
 //= require pagedown_bootstrap/extra
 //= require pagedown_bootstrap/initializer
-
--- lib/pagedown_bootstrap/simple_form.rb
+++ lib/pagedown_bootstrap/simple_form.rb
@@ -1,11 +1,12 @@
 class PagedownInput < SimpleForm::Inputs::TextInput
+  include ActionView::Context
   include ActionView::Helpers::TagHelper
   def input
-    out = tag(:div, :id=> "wmd-button-bar-#{attribute_name}")
-    out << @builder.text_area(attribute_name, input_html_options.merge(:class => 'wmd-input', :id => "wmd-input-#{attribute_name}"))
-    if input_html_options[:preview]
-      out << tag(:div, :id=>"wmd-preview-#{attribute_name}", :class=>"wmd-preview")
+    content_tag(:div, :id=> "wmd-button-bar-#{attribute_name}") do
+      html = []
+      html << @builder.text_area(attribute_name, input_html_options.merge(:class => 'wmd-input', :id => "wmd-input-#{attribute_name}"))
+      html << tag(:div, :id=>"wmd-preview-#{attribute_name}", :class=>"wmd-preview") if input_html_options[:preview]
+      html.join("\n").html_safe
     end
-    out.html_safe
   end
 end
