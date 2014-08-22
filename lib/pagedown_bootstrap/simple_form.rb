169a2fc434031077f5e37cf1c0cd9fc03e124937
Alexander Kostrov <bombazook@gmail.com>
2014-08-22 05:42:13 +0400

    html5 compatible tags for simple_form

--- lib/pagedown_bootstrap/simple_form.rb
+++ lib/pagedown_bootstrap/simple_form.rb
@@ -2,11 +2,11 @@ class PagedownInput < SimpleForm::Inputs::TextInput
   include ActionView::Context
   include ActionView::Helpers::TagHelper
   def input
-    content_tag(:div, :id=> "wmd-button-bar-#{attribute_name}") do
+    content_tag(:div, :id => "wmd-button-bar-#{attribute_name}") do
       html = []
       html << @builder.text_area(attribute_name, input_html_options.merge(:class => 'wmd-input', :id => "wmd-input-#{attribute_name}"))
       if input_html_options[:preview]
-        html << content_tag(:div, :class => "form-group"){tag(:div, :id=>"wmd-preview-#{attribute_name}", :class=>"wmd-preview") }
+        html << content_tag(:div, :class => "form-group"){ content_tag(:div, :id=>"wmd-preview-#{attribute_name}", :class=>"wmd-preview"){""} }
       end
       html.join("\n").html_safe
     end
